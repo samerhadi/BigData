0996565254cc1fd2f58a05c008ea0ff89f920a84
Samerhadi1997 <samer@LAPTOP-68F0URJC>
2019-01-29 13:54:34 +0100

    Skapa Project Skapa Klasser

---
+++ BigData/Models/Bokningsystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigData.Models
+{
+    //skapar klassen för bokningtjänsten
+    public class Bokningsystem
+
+    {
+        public int BookningSystemId { get; set; }
+        public string SystemName { get; set; }
+        public string SystemDescription { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Website { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactEmail { get; set; }
+        public int ContactPhone { get; set; }
+        public string Adress { get; set; }
+        public string LatitudeAndLongitude { get; set; }
+        public string PostaICode {get; set;}
+        public string City { get; set; }
+
+    }
+}
