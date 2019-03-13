e799b559b81e7100ba80e2b847d248c01c54241e
Torlindo <Torlindo@LAPTOP-0A1C21UI>
2019-03-13 14:04:54 +0100

    DateTimePicker

---
+++ DataLogic/Models/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataLogic.Entities;
+
+namespace DataLogic.Models
+{
+    public class Class1
+    {
+        public DateTime Tid { get; set; }
+    }
+
+    public class whatever
+    {
+        public DateTime Tid { get; set; }
+        public BookingSystemEntity BookingSystem { get; set; }
+    }
+}
