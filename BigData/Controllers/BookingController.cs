75ee009b99cb4a4f64e785e6a57a0008866a7a1a
Torlindo <Torlindo@LAPTOP-0A1C21UI>
2019-03-15 11:33:59 +0100

    Byter controller för allt som har med booking

--- BigData/Controllers/BookingController.cs
+++ BigData/Controllers/BookingController.cs
@@ -12,10 +12,53 @@ namespace BigData.Controllers
 {
     public class BookingController : BaseController
     {
-        // GET: Booking
-        public ActionResult BookTime()
+        // GET: BookTime första gången den besöks
+        public ActionResult BookTime(int id)
         {
+            var findTimeModel = new FindTimeModel();
+            findTimeModel.BookingSystem = db.BookingSystems.Find(id);
+            findTimeModel.DateChoosen = false;
+            return View(findTimeModel);
+        }
+
+        //GET: BookTime med en vald dag
+        [HttpPost]
+        public ActionResult BookTime(FindTimeModel findTimeModel)
+        {
+            findTimeModel.BookingSystem = db.BookingSystems.Find(findTimeModel.BookingSystem.BookningSystemId);
+            findTimeModel.DateChoosen = true;
+            findTimeModel.ListOfTimes = CreateListOfTimes();
+            return View(findTimeModel);
+        }
+
+        //Returnerar en lista med alla tider
+        public List<string> CreateListOfTimes()
+        {
+            var listOfTimes = new List<string>();
+            int startTid = 8;
+            string skapaTid;
+            for (int i = 0; i <= 8; i++)
+            {
+                skapaTid = Convert.ToString(startTid) + ":00-" + Convert.ToString(startTid + 1) + ":00";
+                listOfTimes.Add(skapaTid);
+                startTid++;
+            }
+            return listOfTimes;
+        }
+
+        //GET: TimeBooked
+        public ActionResult TimeBooked(FindTimeModel findTimeModel, string time, int id)
+        {
+            findTimeModel.BookingSystem = db.BookingSystems.Find(id);
+            findTimeModel.ChoosenTime = time;
+            SaveBookedTime(findTimeModel);
             return View();
+        }
+
+        //Sparar en vald tid i databasen
+        public void SaveBookedTime(FindTimeModel findTimeModel)
+        {
+
         }
     }
 }
